8bc152f2e3c57b41d756b7e48e32b0f16f831ff6
ansushina <nastya.sushina@yandex.ru>
2022-10-25 22:23:33 +0300

    add materal and simple example

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthPageComponent } from './components/auth-page/auth-page.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -12,6 +18,12 @@ import { AuthRoutingModule } from './auth-routing.module';
   imports: [
     CommonModule,
     AuthRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
-export class AuthModule { }
+export class AuthModule {}
