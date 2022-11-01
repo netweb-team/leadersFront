0380e19768b40a50cd2107cd122b6490a3bb84b3
ansushina <nastya.sushina@yandex.ru>
2022-11-02 02:06:19 +0300

    add mock data for table

--- src/app/start-table/components/start-table/start-table.component.ts
+++ src/app/start-table/components/start-table/start-table.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, map } from 'rxjs';
+import { PoolsTableRow } from 'src/app/core/models/pool';
+import { PoolMock } from 'src/app/core/models/pool.mock';
 import { Flat } from '../../models/start-table';
 
 @Component({
@@ -11,7 +13,12 @@ export class StartTableComponent implements OnInit {
 
   public displayedColumns = ['check', 'index', 'address', 'rooms', 'floors'];
 
-  public dataSource: Observable<unknown[]>= of([{}, {}, {} ]);
+  public dataSource: Observable<Array<PoolsTableRow & {index: number}>>= of([...PoolMock]).pipe(
+    map(pools => pools.map((poolRow, index) => ({
+      ...poolRow,
+      index: index + 1,
+    })))
+  );
 
   constructor() { }
 
