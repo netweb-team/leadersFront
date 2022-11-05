b28cec1cfac34b6774ce52bdc8a9fbbade2973e4
ansushina <nastya.sushina@yandex.ru>
2022-11-06 00:28:09 +0300

    simple archive page

--- src/app/archive/components/archive-page/archive-page.component.ts
+++ src/app/archive/components/archive-page/archive-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ResultTableService } from 'src/app/result-table/services/result-table.service';
 
 @Component({
   selector: 'app-archive-page',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArchivePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public readonly resultTable: ResultTableService,
+  ) { }
 
   ngOnInit(): void {
   }
